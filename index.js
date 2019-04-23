9c8d46f5bb83f1a67a88b1626f158258e8076151
oswaldo  martinez <oswaldomartinez@MacBook-Pro-de-oswaldo.local>
2019-04-23 14:03:29 -0500

    basic resources for demo

--- index.js
+++ index.js
@@ -21,24 +21,3 @@ const tyrion = {
 }
 const characters = [jon, cersei, daenerys, sansa, tyrion]
 
-// nodos
-
-const button = document.querySelector('button')
-const image = document.querySelector('img')
-const name = document.querySelector('h2')
-
-const chooseCharacter = () => {
-
-    const interval = setInterval(() => {
-        const character = Math.floor(Math.random() * characters.length)
-        image.src = characters[character].image
-        name.innerText = characters[character].name
-    }, 500)
-
-    setTimeout(() => {
-        clearInterval(interval)
-        image.classList.add('winner')
-    }, 3000)
-}
-
-button.onclick = chooseCharacter
